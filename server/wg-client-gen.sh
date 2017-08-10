d221422f3f7d138160fabfc3b46134fb89d2b6f5
Leif Sawyer <leif@denali.net>
2017-08-10 13:08:05 -0800

    extend capability with 'create' and 'remove'  options

--- server/wg-client-gen.sh
+++ server/wg-client-gen.sh
@@ -21,89 +21,87 @@ ip2dec () {
 }
 
 
+create() {
+	echo -n "client name: "
+	read client
 
-echo -n "client name: "
-read client
+	echo -n "client public key: "
+	read pk
 
-echo -n "client public key: "
-read pk
+	echo -n "Generating config..."
 
-echo -n "Generating config..."
+	if [ -r ${WGDIR}/client-cfgs/${client}.conf ]
+	then
+		echo -e "failed.\n ${client} configuration already exists."
+		ls -alF ${WGDIR}/client-cfgs/${client}.conf
+		exit 1
+	fi
 
-[[ -r /etc/default/wgserver ]] && . /etc/default/wgserver
+	IP4SNb=${WANv4##*/}			# subnet bits
+	IP4SNs=$(( 2 ** ( 32 - IP4SNb) ))	# Max addresses
 
-if [ -r ${WGDIR}/client-cfgs/${client}.conf ]
-then
-	echo -e "failed.\n ${client} configuration already exists."
-	ls -alF ${WGDIR}/client-cfgs/${client}.conf
-	exit 1
-fi
+	SERIAL=$(grep '\.4wan' ${WGDIR}/clients.conf | wc -l)
+	SERIAL=$((SERIAL + 2))    # get the next sequence, add an additional one to the offset for zero's based counting.
+	if [ ${SERIAL} -gt ${IP4SNs} ];
+	then
+		echo "failed:  out of IP4 pool space"
+		exit 1
+	fi
 
-IP4SNb=${WANv4##*/}			# subnet bits
-IP4SNs=$(( 2 ** ( 32 - IP4SNb) ))	# Max addresses
+	# word-based math is easy!
+	IP4SN=${WANv4//*\//}
+	ip2dec "${WANv4%/*}"
+	IP4d=${MYi2d}
+	IP4d=$(( $IP4d + $SERIAL ))
+	dec2ip "${IP4d}"
+	IP4="${MYd2i}/${IP4SNb}"
 
-SERIAL=$(grep '\.4wan' ${WGDIR}/clients.conf | wc -l)
-SERIAL=$((SERIAL + 2))    # get the next sequence, add an additional one to the offset for zero's based counting.
-if [ ${SERIAL} -gt ${IP4SNs} ];
-then
-	echo "failed:  out of IP4 pool space"
-	exit 1
-fi
+	SNv6=${WANv6%::*}
+	SNv6=${SNv6##*:}
+	SNv6D=$((16#${SNv6}))
+	SNv6D=$((SNv6D + ${SERIAL}))	# use the IPv4 serial as the IPv6 serial, for reservations
+	nSNv6=$(printf "%x" ${SNv6D})
+	WANv6=${WANv6//:$SNv6::/:$nSNv6::}
 
-# word-based math is easy!
-IP4SN=${WANv4//*\//}
-ip2dec "${WANv4%/*}"
-IP4d=${MYi2d}
-IP4d=$(( $IP4d + $SERIAL ))
-dec2ip "${IP4d}"
-IP4="${MYd2i}/${IP4SNb}"
+	###
 
-SNv6=${WANv6%::*}
-SNv6=${SNv6##*:}
-SNv6D=$((16#${SNv6}))
-SNv6D=$((SNv6D + ${SERIAL}))	# use the IPv4 serial as the IPv6 serial, for reservations
-nSNv6=$(printf "%x" ${SNv6D})
-WANv6=${WANv6//:$SNv6::/:$nSNv6::}
+	# half-word based math is harder.  :-(
+	# for /56's out of a /40?  yea.....
+	#  1234:5678:9abc:def0:: ..
+	#             ^ ^  ^
+	#         40-/  |  \-56
+	#              48
+	# so:
+	# LANv6=1234:5678:9aXX:XXhh::/40
+	# the 16bits of [XX:XX] are what we can change
+	# the previous bits are network, and the hh::...  are all downstream
+	#  This isn't really pretty, but it works.  Ish.
+	SNv6=${LANv6%::*}	# 1234:5678:9aXX:XXhh
+	SNv6a=${SNv6%??:*}	# 1234:5678:9a
+	SNv6b=${SNv6#*:*:??}	# XX:XXhh
+	SNv6b=${SNv6b%??}	# XX:XX
+	SNv6b=${SNv6b//:/}	# XXXX
+	SNv6D=$((16#${SNv6b}))
+	SNv6D=$((SNv6D + ${SERIAL}))	# use the IPv4 serial as the IPv6 serial, for reservations
+	nSNv6=$(printf "%04x" ${SNv6D})
+	nSNv62=${nSNv6#??}
+	nSNv61=${nSNv6%??}
+	LANv6="${SNv6a}${nSNv61}:${nSNv62}00::/56"
 
-###
+	DEV=$(awk 'BEGIN { IGNORECASE=1 } /^[a-z0-9]+[ \t]+00000000/ { print $1 }' /proc/net/route 2>/dev/null)
+	MyIntIPv4=$(ip -4 addr show dev ${DEV} scope global)
+	MyIntIPv4=${MyIntIPv4##*inet }
+	MyIntIPv4=${MyIntIPv4%%brd*}
+	MyIntIPv4=${MyIntIPv4%%/*}
 
-# half-word based math is harder.  :-(
-# for /56's out of a /40?  yea.....
-#  1234:5678:9abc:def0:: ..
-#             ^ ^  ^
-#         40-/  |  \-56
-#              48
-# so:
-# LANv6=1234:5678:9aXX:XXhh::/40
-# the 16bits of [XX:XX] are what we can change
-# the previous bits are network, and the hh::...  are all downstream
-#  This isn't really pretty, but it works.  Ish.
-SNv6=${LANv6%::*}	# 1234:5678:9aXX:XXhh
-SNv6a=${SNv6%??:*}	# 1234:5678:9a
-SNv6b=${SNv6#*:*:??}	# XX:XXhh
-SNv6b=${SNv6b%??}	# XX:XX
-SNv6b=${SNv6b//:/}	# XXXX
-SNv6D=$((16#${SNv6b}))
-SNv6D=$((SNv6D + ${SERIAL}))	# use the IPv4 serial as the IPv6 serial, for reservations
-nSNv6=$(printf "%04x" ${SNv6D})
-nSNv62=${nSNv6#??}
-nSNv61=${nSNv6%??}
-LANv6="${SNv6a}${nSNv61}:${nSNv62}00::/56"
+	echo "${client}.4wan = ${IP4}" >> ${WGDIR}/clients.conf
+	echo "${client}.6wan = ${WANv6%/*}/64" >> ${WGDIR}/clients.conf
+	echo "${client}.6lan = ${LANv6%/*}/56" >> ${WGDIR}/clients.conf
+	echo ""  >> ${WGDIR}/clients.conf
 
-DEV=$(awk 'BEGIN { IGNORECASE=1 } /^[a-z0-9]+[ \t]+00000000/ { print $1 }' /proc/net/route 2>/dev/null)
-MyIntIPv4=$(ip -4 addr show dev ${DEV} scope global)
-MyIntIPv4=${MyIntIPv4##*inet }
-MyIntIPv4=${MyIntIPv4%%brd*}
-MyIntIPv4=${MyIntIPv4%%/*}
+	mkdir -p ${WGDIR}/client-cfgs/
 
-echo "${client}.4wan = ${IP4}" >> ${WGDIR}/clients.conf
-echo "${client}.6wan = ${WANv6%/*}/64" >> ${WGDIR}/clients.conf
-echo "${client}.6lan = ${LANv6%/*}/56" >> ${WGDIR}/clients.conf
-echo ""  >> ${WGDIR}/clients.conf
-
-mkdir -p ${WGDIR}/client-cfgs/
-
-echo "[Interface]
+	echo "[Interface]
 PrivateKey = [pending]
 Address = ${IP4}/${IP4SN}
 ListenPort = 51820
@@ -121,8 +119,84 @@ AllowedIPs = 0.0.0.0/0, ::/0
 PersistentKeepalive = 25
 " >${WGDIR}/client-cfgs/${client}.conf
 
-echo "${pk}" > ${WGDIR}/clients/${client}.publickey
+	echo "${pk}" > ${WGDIR}/clients/${client}.publickey
 
-echo "...done"
+	echo "...done"
+}
+### end of create()
+
+remove() {
+	ERR=0
+
+	echo -n "remove which client? "
+	read client
+
+	if [ -n "${client}" -a -r "${WGDIR}/client-cfgs/${client}.conf" ]
+	then
+		ls -alF ${WGDIR}/client-cfgs/${client}.conf
+	else
+		echo "Can't find config file for ${client:-(null)}"
+		ERR=$((${ERR} + 1))
+	fi
+
+	if [ -n "${client}" -a -r "${WGDIR}/clients/${client}.publickey" ]
+	then
+		ls -alF "${WGDIR}/clients/${client}.publickey"
+	else
+		echo "Can't find public keyfile for ${client:-(null)}"
+		ERR=$((${ERR} + 1))
+	fi
+
+	if [ -n "${client}" -a -n "$(grep -iE """${client}\.[46][lw]an""" ${WGDIR}/clients.conf)" ]
+	then
+		grep -iE "${client}\.[46][lw]an" ${WGDIR}/clients.conf
+	else
+		echo "Can't find network config for ${client:-(null)}"
+		ERR=$((${ERR} + 1))
+	fi
+
+	if [ ${ERR} -gt 0 ]
+	then
+		echo "one or more errors occured.  Please clean up manually"
+		exit 1
+	fi
+	
+	echo -en "\nReally remove client ${client}? "
+	read remove
+	
+	remove=${remove,,}
+	remove=${remove%??}
+
+	if [ "${remove:-n}" = "y" ]
+	then
+		sed -i "/${client}.[46][lw]an/ { d; }" ${WGDIR}/clients.conf
+		rm -f "${WGDIR}/clients/${client}.publickey" "${WGDIR}/client-cfgs/${client}.conf"
+		
+		echo "client ${client} has been removed"
+	else
+		echo "client not removed"
+		
+	fi
+	exit 0
+}
+
+###  MAIN ###
+
+[[ -r /etc/default/wgserver ]] && . /etc/default/wgserver
+if [ -z "${WGDIR}" -o ! -d "${WGDIR}" ];
+then
+	echo "Missing global config file '/etc/default/wgserver'"
+	echo "Can't continue"
+	exit 1
+fi
+
+case $1 in
+	create) create
+		;;
+	remove) remove
+		;;
+	*)	echo "Usage: ${0} [create|remove]"
+		;;
+esac
 
 exit 0
